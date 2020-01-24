bb421843e3619b1afa972f3cd821616c79e9963b
Andres Gomez <andresgomezjr89@gmail.com>
2020-01-24 05:39:34 -0500

    added gist exporter

---
+++ Assets/UnityTools/EditorTools/Editor/GistExporter.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using System;
+using System.Text;
+using UnityEngine.Networking;
+namespace UnityTools.EditorTools {
+
+	public sealed class GistExporter : EditorWindow
+	{   
+        const string USER = "tiredamage42";
+
+		public sealed class ResultData
+		{
+			public string html_url = string.Empty;
+		}
+
+		static Vector2 SIZE = new Vector2( 380, 182 );
+
+		string m_path, m_password, m_description, m_filename, response;
+		bool m_isPublic;
+        int phase;
+
+		[MenuItem( "Assets/Export Gist", true )]
+		static bool CanOpen() {
+			if (Selection.objects.Length != 1)
+			    return false;
+			return Selection.activeObject is TextAsset || Selection.activeObject is MonoScript;
+		}
+
+		[MenuItem( "Assets/Export Gist", priority = 99999 )]
+		static void Open () {
+			var win = GetWindow<GistExporter>( true, "Export Gist" );
+			win.Init( Selection.activeObject );
+		}
+
+		void Init ( UnityEngine.Object target ) {
+			m_path = AssetDatabase.GetAssetPath( target );
+			m_filename = Path.GetFileName( m_path );
+			minSize = SIZE;
+			maxSize	= SIZE;
+			position.Set( position.x, position.y, SIZE.x, SIZE.y );
+			m_description = null;
+            phase = 0;
+            response = null;
+		}
+
+		void OnGUI() {
+			m_password = EditorGUILayout.TextField( "Password", m_password );
+			m_description = EditorGUILayout.TextField( "Description", m_description );
+			m_isPublic = EditorGUILayout.Toggle( "Public", m_isPublic );
+			m_filename = EditorGUILayout.TextField( "Filename", m_filename );
+
+			GUI.enabled = !string.IsNullOrEmpty( m_password ) && !string.IsNullOrEmpty( m_filename );
+			if ( GUILayout.Button( "Export Gist" ) )
+				Post();
+			GUI.enabled = true;
+
+			EditorGUILayout.Space();
+            if (phase == 2)
+				EditorGUILayout.LabelField( "Done!" );
+			else if ( phase == -1 )
+				EditorGUILayout.HelpBox( response, MessageType.Error );
+			else if ( phase == 1 )
+				EditorGUILayout.LabelField( "Posting..." );
+		}
+
+		void Post() {
+            phase = 1;
+            response = null;
+			if ( !File.Exists( m_path ) ) {
+                phase = -1;
+				response = "File Not Found " + m_path;
+				return;
+			}
+			var sr = new StreamReader( m_path );
+			var content = sr.ReadToEnd();
+			sr.Close();
+
+			var coroutine = Create (m_password, m_description, m_isPublic, m_filename, content,
+				onComplete : json => {
+                    phase = 2;
+					response = (JsonUtility.FromJson<ResultData>( json ) ?? new ResultData()).html_url;
+					Repaint();
+                    Application.OpenURL( response );
+				},
+				onError : error => {
+					phase = -1;
+					response = error;
+					Repaint();
+				}
+			);
+			EditorCoroutine.Start( coroutine );
+		}
+
+		static IEnumerator Create(string password, string description, bool isPublic, string filename, string content, Action<string> onComplete = null, Action<string> onError = null) {
+			var enc = Encoding.UTF8;
+			var data = enc.GetBytes( CreateDataRaw( description, isPublic, filename, content ) );
+			var headers = new Dictionary<string, string> {
+				{ "Authorization", string.Format( "Basic {0}", Convert.ToBase64String( enc.GetBytes( string.Format( "{0}:{1}", USER, password ) ) ) )	},
+				{ "User-Agent", "GistSharp"	},
+			};
+			using (var www = new WWW( "https://api.github.com/gists", data, headers )) {
+                yield return www;
+                if ( !string.IsNullOrEmpty( www.error ) ) {
+                    if ( onError != null )
+                        onError( www.error );
+                    yield break;
+                }
+                if ( onComplete != null )
+                    onComplete( www.text );
+            }
+		}
+		static string CreateDataRaw (string description, bool isPublic, string filename, string content) {
+			var dataRaw = @"{""description"":""" + description + @""","
+				+ @"""public"":" + isPublic.ToString().ToLower() + ","
+				+ @"""files"":{""" + filename + @""":{"
+				+ @"""content"":""" + Escape( content ) + @"""}}}";
+			return dataRaw;
+		}
+		static string Escape( string value ) {
+			return value.Replace( @"\", @"\\" ).Replace( @"""", @"\""" ).Replace( "\t", @"\t" ).Replace( @"
+", @"\n" );
+		}
+		
+	}
+
+}
---
+++ Assets/UnityTools/EditorTools/EditorCoroutine.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityTools.EditorTools {
+    public class EditorCoroutine : IDisposable {
+        public static EditorCoroutine Start(IEnumerator routine) {
+            return new EditorCoroutine(routine).Start();
+        }
+        static readonly FieldInfo WaitForSeconds_m_Seconds = typeof(UnityEngine.WaitForSeconds).GetField("m_Seconds", BindingFlags.Instance | BindingFlags.NonPublic);
+        static IEnumerator WaitForSeconds(object subroutine) {
+            var waitForSeconds = subroutine as UnityEngine.WaitForSeconds;
+            if (waitForSeconds == null)
+                return null;
+
+            var seconds =(float)WaitForSeconds_m_Seconds.GetValue(waitForSeconds);
+            return WaitForSeconds(seconds);
+        }
+
+        static IEnumerator WaitForSeconds(float seconds) {
+            var end = DateTime.Now + TimeSpan.FromSeconds(seconds);
+            do yield return null; while (DateTime.Now < end);
+        }
+
+        readonly Stack<IEnumerator> _stack = new Stack<IEnumerator>();
+
+        public EditorCoroutine(IEnumerator routine) {
+            Push(routine);
+        }
+        void Push(object subroutine) {
+            Push(subroutine as IEnumerator ?? WaitForSeconds(subroutine));
+        }
+
+        void Push(IEnumerator subroutine) {
+            if (subroutine != null) {
+                // Debug.Log("Push To Stack");
+                _stack.Push(subroutine);
+            }
+            else {
+                // Debug.Log("Subroutine null");
+            }
+        }
+        
+        void IDisposable.Dispose() {
+            Stop();
+        }
+
+        public EditorCoroutine Start() {
+            if (_stack.Count > 0) {
+                EditorApplication.update -= Update;
+                EditorApplication.update += Update;
+            }
+            return this;
+        }
+
+        public void Stop() {
+            EditorApplication.update -= Update;
+        }
+
+        void Update() {
+            try { 
+                MoveNext(); 
+            }
+            catch (Exception ex) { 
+                Stop(); 
+                Debug.LogError(ex); 
+            }
+        }
+        void MoveNext() {
+            // Debug.Log("Move Next");
+            var routine = _stack.Peek();
+            if (routine.MoveNext()) {
+                // Debug.Log("Push In Move Next");
+                Push(routine.Current);
+            }
+            else
+                Pop();
+        }
+        void Pop() {
+            _stack.Pop();
+            if (_stack.Count == 0) 
+                Stop();
+        }
+    }
+}
